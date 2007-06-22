cb0bad5d9d0f2b9eea4f08b4ba87ab0ded118877
cdwiegand <cdwiegand@646a8dc2-ab0d-0410-92fd-c00d52ce5a70>
2007-06-22 02:55:36 +0000

    1.07b, fixed the version in globals.vb
    git-svn-id: https://svn.code.sf.net/p/pocketposter/code/branches/PocketPoster_1.0@47 646a8dc2-ab0d-0410-92fd-c00d52ce5a70

--- PocketPoster/PocketPoster/Globals.vb
+++ PocketPoster/PocketPoster/Globals.vb
@@ -2,8 +2,8 @@ Imports System.net
 
 Module Globals
 
-    Public MyVersion As String = "1.06"
-    Public MyRevision As Long = 106
+    Public MyVersion As String = "1.07"
+    Public MyRevision As Long = 107
 
     Public mySession As New LJSession ' yeah, I'm cheating...
     Private m_SettingsXML As Xml.XmlDocument = Nothing
